0f4d30ea2f1bf6f651d541802f5c67223a1c57d4
Rhys Evans <rhys.evans@ft.com>
2017-01-25 21:45:59 +0000

    first stab at an n-express wrapper aimed at internal tooling

---
+++ index.js
@@ -0,0 +1,44 @@
+const nExpress = require('@financial-times/n-express');
+const nHandlebars = require('@financial-times/n-handlebars');
+const path = require('path');
+
+const handlebars = function ({app, directory, options}) {
+	const viewsDirectory = options.viewsDirectory || '/views';
+	const partialsDir = [
+		directory + viewsDirectory + '/partials',
+	];
+
+	if (options.partialsDirectory) {
+		partialsDir.push(options.partialsDirectory);
+	}
+
+	return nHandlebars(app, {
+		partialsDir,
+		defaultLayout: options.defaultLayout || false,
+		layoutsDir: options.layoutsDir || path.join(__dirname, 'layouts'),
+		helpers: options.helpers || {},
+		directory: directory,
+		viewsDirectory: viewsDirectory
+	})
+}
+
+module.exports = options => {
+	options = options || {};
+
+	const {app, meta, addInitPromise} = nExpress.getAppContainer({
+		systemCode: options.systemCode,
+		healthChecks: options.healthchecks || []
+	})
+
+	addInitPromise(handlebars({
+		app,
+		directory: meta.directory,
+		options
+	}));
+
+	return app;
+}
+
+module.exports.Router = nExpress.Router;
+module.exports.static = nExpress.static;
+module.exports.metrics = nExpress.metrics;
